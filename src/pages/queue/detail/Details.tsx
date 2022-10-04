f95f5b9fc444f8833ee4f726fe08b9ec3ad9d2c8
hphuocthanh <phuocthanhqt113@gmail.com>
2022-10-04 12:39:49 +0700

    💦 add qr code search

--- src/pages/queue/detail/Details.tsx
+++ src/pages/queue/detail/Details.tsx
@@ -1,29 +1,27 @@
-import { useGetQueueByIdQuery } from '@/store/queue/api'
-import { formatCurrency } from '@/utils/formats'
-import { translateEnumInsuranceStatus } from '@/utils/renderEnums'
-import { Stack, Title, Table, Text } from '@mantine/core'
-import { useParams } from 'react-router-dom'
+import { useGetBillByIdQuery } from "@/store/queue/api";
+import { formatCurrency } from "@/utils/formats";
+import { translateEnumInsuranceStatus } from "@/utils/renderEnums";
+import { Stack, Title, Table, Text } from "@mantine/core";
+import { useParams } from "react-router-dom";
 
 const Details = () => {
-	const { id } = useParams()
-	const { data } = useGetQueueByIdQuery(Number(id), {
+	const { id } = useParams();
+	const { data } = useGetBillByIdQuery(Number(id), {
 		skip: !id,
-	})
+	});
 
-	const rows = data?.details?.map((item, index) => (
-		<tr key={item.id}>
-			<td>{index + 1}</td>
-			<td>{item.operationName}</td>
-			<td>{translateEnumInsuranceStatus(item.insuranceStatus)}</td>
-			<td className="right">
-				{item?.price ? formatCurrency(item.price) : '---'}
-			</td>
-			<td className="right">{item?.quantity ? item.quantity : '---'}</td>
-			<td className="right">
-				{item?.subTotal ? formatCurrency(item.subTotal) : '---'}
-			</td>
-		</tr>
-	))
+	const rows = data?.details?.map(
+		(item, index) => (
+			<tr key={item.id}>
+				<td>{index + 1}</td>
+				<td>{item.operationName}</td>
+				<td>{translateEnumInsuranceStatus(item.insuranceStatus)}</td>
+				<td className="right">{item?.price ? formatCurrency(item.price) : "---"}</td>
+				<td className="right">{item?.quantity ? item.quantity : "---"}</td>
+				<td className="right">{item?.subTotal ? formatCurrency(item.subTotal) : "---"}</td>
+			</tr>
+		),
+	);
 
 	const ths = (
 		<>
@@ -33,12 +31,10 @@ const Details = () => {
 				<th />
 				<th />
 				<th className="right">Tổng cộng</th>
-				<th className="right">
-					{data?.total ? formatCurrency(data.total) : '---'}
-				</th>
+				<th className="right">{data?.total ? formatCurrency(data.total) : "---"}</th>
 			</tr>
 		</>
-	)
+	);
 
 	return (
 		<Stack>
@@ -60,14 +56,14 @@ const Details = () => {
 					<tbody>{rows}</tbody>
 					<tfoot>{ths}</tfoot>
 				</Table>
-				<Stack align={'end'} px="sm">
+				<Stack align={"end"} px="sm">
 					<Text size="sm" italic={true}>
 						{data?.totalInWord}
 					</Text>
 				</Stack>
 			</Stack>
 		</Stack>
-	)
-}
+	);
+};
 
-export default Details
+export default Details;
--- src/pages/queue/detail/index.tsx
+++ src/pages/queue/detail/index.tsx
@@ -1,127 +1,100 @@
-import BackButton from '@/components/Button/BackButton'
-import {
-	useConfirmBillMutation,
-	useGetQueueByIdQuery,
-	useInvalidateBillMutation,
-} from '@/store/queue/api'
-import { formatCurrency } from '@/utils/formats'
-import { Badge } from '@mantine/core'
-import {
-	Paper,
-	Stack,
-	Divider,
-	Group,
-	Button,
-	Text,
-	LoadingOverlay,
-} from '@mantine/core'
-import { openConfirmModal } from '@mantine/modals'
-import { showNotification } from '@mantine/notifications'
-import { IconCheck, IconTrash } from '@tabler/icons'
-import { useNavigate, useParams } from 'react-router-dom'
-import Details from './Details'
-import Info from './Info'
+import BackButton from "@/components/Button/BackButton";
+import { useConfirmBillMutation, useGetBillByIdQuery, useInvalidateBillMutation } from "@/store/queue/api";
+import { formatCurrency } from "@/utils/formats";
+import { Badge } from "@mantine/core";
+import { Paper, Stack, Divider, Group, Button, Text, LoadingOverlay } from "@mantine/core";
+import { openConfirmModal } from "@mantine/modals";
+import { showNotification } from "@mantine/notifications";
+import { IconCheck, IconTrash } from "@tabler/icons";
+import { useNavigate, useParams } from "react-router-dom";
+import Details from "./Details";
+import Info from "./Info";
 
 const QueueDetail = () => {
-	const { id: queueId } = useParams()
-	const navigate = useNavigate()
+	const { id: queueId } = useParams();
+	const navigate = useNavigate();
 
-	const { data } = useGetQueueByIdQuery(Number(queueId), {
+	const { data } = useGetBillByIdQuery(Number(queueId), {
 		skip: !queueId,
-	})
-	const [invalidateBillMutation, { isLoading: isLoadingInvalidate }] =
-		useInvalidateBillMutation()
-	const [confirmBillMutation, { isLoading: isLoadingConfirm }] =
-		useConfirmBillMutation()
+	});
+	const [invalidateBillMutation, { isLoading: isLoadingInvalidate }] = useInvalidateBillMutation();
+	const [confirmBillMutation, { isLoading: isLoadingConfirm }] = useConfirmBillMutation();
 
 	const openDeleteModal = () =>
 		openConfirmModal({
-			title: 'Hủy hóa đơn',
+			title: "Hủy hóa đơn",
 			centered: true,
-			children: (
-				<Text size="sm">
-					Bạn có chắc chắn muốn hủy hóa đơn này? Thao tác này không thể hoàn
-					tác.
-				</Text>
-			),
-			labels: { confirm: 'Xác nhận hủy', cancel: 'Quay lại' },
-			confirmProps: { color: 'red' },
+			children: <Text size="sm">Bạn có chắc chắn muốn hủy hóa đơn này? Thao tác này không thể hoàn tác.</Text>,
+			labels: { confirm: "Xác nhận hủy", cancel: "Quay lại" },
+			confirmProps: { color: "red" },
 			onConfirm: () =>
 				invalidateBillMutation(Number(queueId))
 					.unwrap()
 					.then(() => {
 						showNotification({
-							title: 'Hủy thành công',
+							title: "Hủy thành công",
 							message: <Text>Bạn đã hủy hóa đơn thành công</Text>,
-							color: 'cyan',
-						})
-						navigate('/')
+							color: "cyan",
+						});
+						navigate("/");
 					})
 					.catch(() => {
 						showNotification({
-							title: 'Lỗi',
+							title: "Lỗi",
 							message: <Text>Hủy hóa đơn không thành công</Text>,
-							color: 'red',
-						})
+							color: "red",
+						});
 					}),
-		})
+		});
 
 	const openModal = () =>
 		openConfirmModal({
-			title: 'Xác nhận hóa đơn',
+			title: "Xác nhận hóa đơn",
 			centered: true,
 			children: (
 				<Text size="sm">
-					Bạn đang xác nhận hóa đơn của{' '}
-					<Text span={true} weight={500}>
+					Bạn đang xác nhận hóa đơn của <Text span={true} weight={500}>
 						{data?.patientName}
-					</Text>{' '}
-					với tổng tiền là{' '}
-					<Text span={true} weight={500}>
-						{data?.total ? formatCurrency(data.total) : '---'}
-					</Text>{' '}
+					</Text>
+					với tổng tiền là <Text span={true} weight={500}>
+						{data?.total ? formatCurrency(data.total) : "---"}
+					</Text>
 					<Text span={true} italic={true}>
-						({data?.totalInWord || '---'})
+						({data?.totalInWord || "---"})
 					</Text>
 					.
 				</Text>
 			),
-			labels: { confirm: 'Xác nhận', cancel: 'Quay lại' },
+			labels: { confirm: "Xác nhận", cancel: "Quay lại" },
 			onConfirm: () =>
 				confirmBillMutation(Number(queueId))
 					.unwrap()
 					.then(() => {
 						showNotification({
-							title: 'Xác nhận thành công',
+							title: "Xác nhận thành công",
 							message: <Text>Bạn đã hủy hóa đơn thành công</Text>,
-						})
-						navigate('/')
+						});
+						navigate("/");
 					})
 					.catch(() => {
 						showNotification({
-							title: 'Lỗi',
+							title: "Lỗi",
 							message: <Text>Xác nhận hóa đơn không thành công</Text>,
-							color: 'red',
-						})
+							color: "red",
+						});
 					}),
-		})
+		});
 
 	return (
-		<Stack align={'start'}>
-			<Stack
-				sx={{ flexDirection: 'row', width: '100%' }}
-				align="center"
-				justify={'space-between'}
-				mb="sm"
-				spacing={40}
-			>
+		<Stack align={"start"}>
+			<Stack sx={{ flexDirection: "row", width: "100%" }} align="center" justify={"space-between"} mb="sm" spacing={40}>
 				<BackButton />
 
 				<Badge size="xl" radius="md">
 					Thông tin hóa đơn
 				</Badge>
 			</Stack>
-			<Paper p="md" sx={{ width: '100%', position: 'relative' }}>
+			<Paper p="md" sx={{ width: "100%", position: "relative" }}>
 				<LoadingOverlay visible={isLoadingInvalidate || isLoadingConfirm} />
 				<Stack>
 					<Info />
@@ -139,18 +112,13 @@ const QueueDetail = () => {
 						>
 							Hủy hóa đơn
 						</Button>
-						<Button
-							size="lg"
-							sx={{ width: 200 }}
-							leftIcon={<IconCheck />}
-							onClick={openModal}
-						>
+						<Button size="lg" sx={{ width: 200 }} leftIcon={<IconCheck />} onClick={openModal}>
 							Xác nhận
 						</Button>
 					</Group>
 				</Stack>
 			</Paper>
 		</Stack>
-	)
-}
-export default QueueDetail
+	);
+};
+export default QueueDetail;
